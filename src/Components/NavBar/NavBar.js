cadd3720bb839a0aa822ea77d4ab1e78fdda290c
Kayla Wood <kaylaewood@gmail.com>
2021-07-27 21:40:31 -0600

    Style NavBar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -3,12 +3,24 @@ import { NavLink } from 'react-router-dom';
 
 function NavBar() {
   return (
-    <div className="NavBar">
-      <NavLink to='/home'>Home</NavLink>
-      <NavLink to='/about'>About Us</NavLink>
-      <NavLink to='/whatwedo'>What We Do</NavLink>
-      <NavLink to='/contact'>Contact Us</NavLink>
-    </div>
+    <header className='header'>
+      <section className='contact-header'>
+        <div className='contact-unit'>
+          <h3>EMAIL US</h3>
+          <p>info@commandtesting.com</p>
+        </div>
+        <div className='contact-unit'>
+          <h3>CALL US</h3>
+          <p>(614) 398-0120</p>
+        </div>
+      </section>
+      <nav>
+        <NavLink className='navlink' activeClassName='active-navlink' to='/home'>HOME</NavLink>
+        <NavLink className='navlink' activeClassName='active-navlink' to='/about'>ABOUT US</NavLink>
+        <NavLink className='navlink' activeClassName='active-navlink' to='/whatwedo'>WHAT WE DO</NavLink>
+        <NavLink className='navlink' activeClassName='active-navlink' to='/contact'>CONTACT US</NavLink>
+      </nav>
+    </header>
   );
 }
 
