4cc01e61df31fb74fab5e2eb813a061a1ceea667
Kayla Wood <kaylaewood@gmail.com>
2021-08-26 14:22:29 -0700

    Update styling for success msg

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -56,44 +56,50 @@ class Contact extends Component {
   render() {
     return (
       <div className="contact" ref={this.props.nav}>
-      <form onSubmit={this.sendEmail}>
+      <div className="form-container">
         <h4>REQUEST A QUOTE</h4>
-        { this.state.success && <p className="success">{this.state.success}</p> }
-        { !this.state.success  && <>
-        <input
-          type="text"
-          name="name"
-          placeholder="Your Name"
-          value={this.state.name}
-          onChange={(e) => this.handleChange(e)}
-        />
-        <input
-          type="text"
-          name="phone"
-          placeholder="Your Phone Number"
-          value={this.state.phone}
-          onChange={(e) => this.handleChange(e)}
-        />
-        <input
-          type="text"
-          name="email"
-          placeholder="Your Email Address"
-          value={this.state.email}
-          onChange={(e) => this.handleChange(e)}
-        />
-        <textarea
-          name="message"
-          placeholder="How can we help you?"
-          value={this.state.message}
-          onChange={(e) => this.handleChange(e)}
-        ></textarea>
-        { this.state.error && <p className="error">{this.state.error}</p> }
-        <input
-          type="submit"
-          value="Submit Request"
-        />
-      </>}
-      </form>
+        { this.state.success &&
+          <div className="success-container">
+            <p className="success">{this.state.success}</p>
+          </div>
+        }
+        { !this.state.success  &&
+          <form onSubmit={this.sendEmail}>
+            <input
+              type="text"
+              name="name"
+              placeholder="Your Name"
+              value={this.state.name}
+              onChange={(e) => this.handleChange(e)}
+            />
+            <input
+              type="text"
+              name="phone"
+              placeholder="Your Phone Number"
+              value={this.state.phone}
+              onChange={(e) => this.handleChange(e)}
+            />
+            <input
+              type="text"
+              name="email"
+              placeholder="Your Email Address"
+              value={this.state.email}
+              onChange={(e) => this.handleChange(e)}
+            />
+            <textarea
+              name="message"
+              placeholder="How can we help you?"
+              value={this.state.message}
+              onChange={(e) => this.handleChange(e)}
+            ></textarea>
+            { this.state.error && <p className="error">{this.state.error}</p> }
+            <input
+              type="submit"
+              value="Submit Request"
+            />
+          </form>
+        }
+      </div>
       <img src={truckRear} className="half-page-image" alt="" />
       </div>
     );
