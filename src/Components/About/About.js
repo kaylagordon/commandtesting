6a2188af707c63a0b7bf455d66e587f594734e54
Kayla Wood <kaylaewood@gmail.com>
2023-06-19 16:41:21 -0400

    Update to React 18 and update images

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,13 +1,15 @@
 import './About.css';
+import family from '../../assets/family.jpg';
+import logo from '../../assets/logo.png';
 
 const About = ({ nav }) => {
   return (
     <div className="about" ref={nav}>
-      <img src="TBD" className="half-page-image" alt="" />
+      <img src={family} className="half-page-image" alt="aerial bucket" />
       <section className="half-page-section">
-        <img src="logo" className="logo" alt="logo" />
+        <img src={logo} className="logo" alt="logo" />
         <h4>WHO WE ARE</h4>
-        <p>Owner, Cody Stallter, has over 10 years of experience in fire apparatus testing and non-destructive testing.  He is an ASNT Level III in liquid penetrant, magnetic particle, ultrasonic, and visual testing.  Having family in the fire protection services industry, firefighter safety is the highest importance for all personnel at Command. As a company, we strive to perform highest quality of work so that fire department personnel can depend on their life saving equipment.</p>
+        <p>Owner, Cody Stallter, has over 10 years of experience in fire apparatus testing and non-destructive testing. He is an ASNT Level III in liquid penetrant, magnetic particle, ultrasonic, and visual testing. Having family in the fire protection services industry, firefighter safety is the highest importance for all personnel at Command. As a company, we strive to perform highest quality of work so that fire department personnel can depend on their life saving equipment.</p>
       </section>
     </div>
   );
--- src/Components/WhatWeDo/WhatWeDo.js
+++ src/Components/WhatWeDo/WhatWeDo.js
@@ -1,27 +1,32 @@
 import './WhatWeDo.css';
+import logo from '../../assets/logo.png';
+import aerial from '../../assets/aerial_bucket_square.jpg';
+import ladder from '../../assets/ground_ladder_square.jpg';
+import pump from '../../assets/pump_testing_square.jpg';
+import weld from '../../assets/welds_square.jpg';
 
 const WhatWeDo = ({ nav }) => {
   return (
     <div className="WhatWeDo" ref={nav}>
-      <img src="logo" className="logo" alt="" />
+      <img src={logo} className="logo" alt="" />
       <h4>WHAT WE DO</h4>
       <section className="offer-bundle">
-        <img src="TBD" className="offer-image" alt="" />
+        <img src={aerial} className="offer-image" alt="" />
         <h5>Aerial Testing</h5>
         <p className="offer-text">Aerial certification testing in accordance with NFPA 1901 & 1911</p>
       </section>
       <section className="offer-bundle">
-        <img src="TBD" className="offer-image" alt="" />
+        <img src={ladder} className="offer-image" alt="" />
         <h5>Ground Ladder Testing</h5>
         <p className="offer-text">Ground ladder testing in accordance with NFPA 1932</p>
       </section>
       <section className="offer-bundle">
-      <img src="TBD" className="offer-image" alt="" />
+      <img src={pump} className="offer-image" alt="" />
       <h5>Pump Testing</h5>
       <p className="offer-text">Pump testing in accordance with NFPA 1901 & 1911</p>
       </section>
       <section className="offer-bundle">
-        <img src="TBD" className="offer-image" alt="" />
+        <img src={weld} className="offer-image" alt="" />
         <h5>Non-destructive Testing</h5>
         <p className="offer-text">Liquid Penetrant, Magnetic Particle, Ultrasonic, and Visual Testing.  Consulting and NDT Level III services.</p>
       </section>
